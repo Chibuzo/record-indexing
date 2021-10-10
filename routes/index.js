a975a30da487130fc5be0c97d11215c8247db8b7
Chibuzo <chibuzo.henry@gmail.com>
2021-10-10 23:50:40 +0100

    remove test route handler

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const dataService = require('../services/dataService');
-const ChatSession = require('../models').ChatSession;
 
 
 router.get('/', (req, res) => {
     res.status(200).json({ message: 'An Indexing Project' });
-});
-
-router.get('/chats', async (req, res, next) => {
-    try {
-        const chats = await ChatSession.findAll();
-        res.json({ chats });
-    } catch (err) {
-        next(err);
-    }
 });
 
 router.get('/content', async (req, res, next) => {
